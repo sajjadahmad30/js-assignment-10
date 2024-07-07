54e46d1b4b764144775b2085475f4c0cd0fe5d68
sajjadahmad30 <sajjadahmad5578@gmail.com>
2024-07-08 00:32:25 +0500

    Assignments

---
+++ Assignments/script.js
@@ -0,0 +1,47 @@
+// 6. FIND THE SMALLEST NUMBER IN AN ARRAY
+// let numbers=[234,4,5,3,0];
+// let smallestNo= numbers[0];
+
+
+// for(let i=0; i<numbers.length;i++){
+//     if(numbers[i]<smallestNo){
+//         smallestNo=numbers[i];
+//     }
+// } 
+// console.log(`The largest number is = `,smallestNo);
+
+
+// 7. CONVERT CELCIUS TO FAHRENHEIT
+// let celcius = +prompt("enter celius to convert F:")
+// let fahrenheit = (celcius*1.8)+32;
+// console.log("The celcius is convert to fahrenheit :", fahrenheit)
+
+
+
+// 8. CONVERT FAHRENHEIT TO CELCIUS
+
+// let fahrenheit = +prompt("enter fahrenheit :")
+// let celcius  = 5/9 * (fahrenheit-32);
+// console.log("The fahrenheit is convert to celcius :", celcius)
+
+
+
+// 9. REVERSE AN ARRAY
+// let array=[43,432,25,24,5]; 
+// console.log(array)
+// for(let i=array.length-1;i>-1;i--){
+// console.log(`${array[i]}`)
+// }
+
+
+
+// 10. CALCULATE AVERAGE OF AN ARRAY
+// let array=[80,20,200,30,20];
+// console.log(`This a array : ${array}`)
+// let avg=0
+// let sum=0
+// for(let i=0;i<array.length;i++){
+//     sum=array[i]+sum;
+//     avg=sum/array.length;
+// }
+// console.log(`The Average of array is = ${avg}`)
